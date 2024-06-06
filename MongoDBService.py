99d58401166f32b48a6e5926fe3365e4b798ccda
MiguelAlvarezFernandez <144229347+MiguelAlvarezFernandez@users.noreply.github.com>
2024-06-06 07:51:17 +0200

    Add files via upload

---
+++ MongoDBService.py
@@ -0,0 +1,190 @@
+import json
+import math
+import pymongo
+import threading
+import paho.mqtt.client as mqtt
+import time
+from collections.abc import MutableMapping
+import dronekit
+from dronekit import connect, Command, VehicleMode
+from paho.mqtt.client import ssl
+from pymavlink import mavutil
+from pymongo import MongoClient
+import mongoengine
+from mongoengine.fields import ReferenceField
+
+client_mongo = pymongo.MongoClient("mongodb://localhost:27018/")
+db = client_mongo["DEE"]
+collection = db["poligonos"]
+
+def get_poligonos():
+    #poligonos = json.loads(PruebaPoligonos.objects().to_json())
+    escenarios = collection.find()
+    documentos = [doc for doc in escenarios]
+    return {"Escenarios": documentos}
+
+def add_poligono(nuevoPoligono):
+    PoligonoInsertado = collection.insert_one(nuevoPoligono)
+    print("Poligono Insertado")
+
+
+def delete_poligono(nuevoPoligono):
+        print ("delete")
+        # collection.delete_one({"Mapa": nuevoPoligono.get('Mapa')})
+
+
+def eliminar_object_id(escenario):
+
+
+    if 'Escenarios' in escenario:
+        for item in escenario['Escenarios']:
+            if '_id' in item:
+                del item['_id']
+    return escenario
+
+def on_message(client_mqtt, userdata, message):
+    print("helloworld")
+    data = message.payload.decode("utf-8")
+    peticion = message.topic
+    # Ejecutar función específica según los datos recibidos
+    if peticion == "mongo/mobileApp/getPoligonos":
+        Escenarios = get_poligonos()
+        Escenarios_Sin_Id = eliminar_object_id(Escenarios)
+        print ("getpoligonos")
+        client_mqtt.publish("mobileApp/mongo/readPoligonos",json.dumps(Escenarios_Sin_Id))
+
+    elif peticion =="mongo/mobileApp/addPoligonos":
+
+        nuevopoligonoprueba = json.loads(message.payload.decode("utf-8"))
+        add_poligono(nuevopoligonoprueba)
+    elif peticion =="mongo/mobileApp/deletePoligonos":
+
+        nuevopoligonoprueba = json.loads(message.payload.decode("utf-8"))
+        delete_poligono(nuevopoligonoprueba)
+
+
+client_mqtt = mqtt.Client("MongoDBService")
+client_mqtt.on_message = on_message
+client_mqtt.connect("localhost", 8000)
+client_mqtt.subscribe("mongo/#")
+#class PruebaPoligonosEsquema(BaseModel):
+#    Jugadores: int
+#    Poligonos: float
+
+#class PruebaPoligonos(mongoengine.Document):
+#    Jugadores = mongoengine.IntField(required=True)
+#    Poligonos = mongoengine.FloatField(required=True)
+
+#    meta = {'collection': 'poligonos'}
+
+
+
+
+
+
+def on_internal_message(client, userdata, message):
+    global internal_client
+    print("helloworl2")
+    process_message(message, internal_client)
+
+def on_external_message(client, userdata, message):
+    global external_client
+    process_message(message, external_client)
+
+def on_connect(external_client, userdata, flags, rc):
+    if rc==0:
+        print("Connection OK")
+    else:
+        print("Bad connection")
+
+def eliminar_object_id(escenario):
+    if 'Escenarios' in escenario:
+        for item in escenario['Escenarios']:
+            if '_id' in item:
+                del item['_id']
+    return escenario
+
+def MongoService (connection_mode, operation_mode, external_broker, username, password):
+    global op_mode
+    global external_client
+    global internal_client
+    global state
+
+
+
+
+    state = 'disconnected'
+
+    print ('Connection mode: ', connection_mode)
+    print ('Operation mode: ', operation_mode)
+    op_mode = operation_mode
+
+
+#    external_client = mqtt.Client("Mongo_external", transport="websockets")
+#    external_client.on_message = on_external_message
+#    external_client.on_connect = on_connect
+
+    if connection_mode== "global":
+        if external_broker == "hivemq":
+            external_client.connect("broker.hivemq.com", 8000)
+            print('Connected to broker.hivemq.com:8000')
+
+        elif external_broker == "hivemq_cert":
+            external_client.tls_set(ca_certs=None, certfile=None, keyfile=None, cert_reqs=ssl.CERT_REQUIRED,
+                           tls_version=ssl.PROTOCOL_TLS, ciphers=None)
+            external_client.connect("broker.hivemq.com", 8884)
+            print('Connected to broker.hivemq.com:8884')
+
+        elif external_broker == "classpip_cred":
+            external_client.username_pw_set(
+                username, password
+            )
+            external_client.connect("classpip.upc.edu", 8000)
+            print('Connected to classpip.upc.edu:8000')
+
+        elif external_broker == "classpip_cert":
+            external_client.username_pw_set(
+                username, password
+            )
+            external_client.tls_set(ca_certs=None, certfile=None, keyfile=None, cert_reqs=ssl.CERT_REQUIRED,
+                           tls_version=ssl.PROTOCOL_TLS, ciphers=None)
+            external_client.connect("classpip.upc.edu", 8883)
+            print('Connected to classpip.upc.edu:8883')
+        elif external_broker == "localhost":
+            external_client.connect("localhost", 8000)
+            print('Connected to localhost:8000')
+        elif external_broker == "localhost_cert":
+            print('Not implemented yet')
+
+
+
+
+
+    print("Waiting....")
+
+    client_mqtt.loop_start()
+
+    if operation_mode == 'simulation':
+        client_mqtt.loop_forever()
+    else:
+        #external_client.loop_start() #when executed on board use loop_start instead of loop_forever
+        external_client.loop_forever()
+
+
+
+
+if __name__ == '__main__':
+    import sys
+    connection_mode = "local" # global or local
+    operation_mode = "simulation" # simulation or production
+    username = None
+    password = None
+    if connection_mode == 'global':
+       external_broker = sys.argv[3]
+       if external_broker == 'classpip_cred' or external_broker == 'classpip_cert':
+            username = sys.argv[4]
+            password = sys.argv[5]
+    else:
+        external_broker = None
+
+    MongoService(connection_mode,operation_mode, external_broker, username, password)
